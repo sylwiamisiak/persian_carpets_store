9dd6775ece4dff7ec368be493f9f1644514a3750
Sylwia <eaasymi@students.eaaa.dk>
2020-02-26 17:09:32 +0100

    Wiktor's changes

--- js.js
+++ js.js
@@ -64,7 +64,7 @@ let now = new Date();
        ];
 
     console.log ( Month [ now.getMonth() ]);
-    result.innerHTML =  "Hello, today is" + '<br>' + week [ now.getDay() ] + ',' + ' ' + day  [ now.getDay() ] + ' ' + Month  [ now.getMonth() ] + ' ' + "2020" ;
+    result.innerHTML =  "Hello, today is" + '<br>' + '<b>' + week [ now.getDay() ] + ',' + ' ' + day  [ now.getDay() ] + ' ' + Month  [ now.getMonth() ] + ' ' + "2020" + '</b>' ;
 
 
 
@@ -85,6 +85,7 @@ let now = new Date();
                 } else {
                     video.pause();
                 }
+
             }
 
             checkScroll();
@@ -94,43 +95,13 @@ let now = new Date();
 
 
         var carpets = [
-    "History & Use" + '<br>' + "Originally Persian carpets were used by nomadic people for private use as floor coverings, blankets or wall decorations. In trade, the carpets were made as decorative elements for palaces, mosques and houses of respected citizens of the old Persian Empire. This way they also found their way to european royal families and wealthy classes of the population who could afford a genuine Persian carpet." + '<p>' + '<br>' +
+   '<b>' + "History & Use" + '</b>' + '<p>' + "Originally Persian carpets were used by nomadic people for private use as floor coverings, blankets or wall decorations. In trade, the carpets were made as decorative elements for palaces, mosques and houses of respected citizens of the old Persian Empire. This way they also found their way to european royal families and wealthy classes of the population who could afford a genuine Persian carpet." + '</p>' + '<br>' +
 
-    "Knots" + '<br>' + "Depending on the pattern and desired fineness, two types of knots are used: the symmetrical and the asymmetrical knot. Asymmetrically knotted carpets mainly depict figures and floral elements, while the Giordes knot is mostly used for geometric patterns." + '<p>' + '<br>' +
+    '<b>' + "Knots" + '</b>' + '<p>' + "Depending on the pattern and desired fineness, two types of knots are used: the symmetrical and the asymmetrical knot. Asymmetrically knotted carpets mainly depict figures and floral elements, while the Giordes knot is mostly used for geometric patterns." + '<p>' + '<br>' +
 
-    "Colours" + '<br>' + "To achieve the wonderful patterns of Persian carpets, different coloured yarns are needed. These can be dyed by hand and with natural or synthetic pigments. Again, natural dyes are more valuable. Probably the best known natural colour and usually the dominating colour in Persian carpets is madder red, which is obtained from the so-called dyer madder, which has its area of distribution in the Middle East and the eastern Mediterranean. In addition, blue is extracted from the indigo plant, which is now common as the basic colour of jeans, and yellow is often extracted from chamomile. Almost all nuances of the entire colour spectrum can be produced by mixing colours." + '<p>' + '<br>' +
+    '<b>' + "Colours" + '</b>' + '<p>' + "To achieve the wonderful patterns of Persian carpets, different coloured yarns are needed. These can be dyed by hand and with natural or synthetic pigments. Again, natural dyes are more valuable. Probably the best known natural colour and usually the dominating colour in Persian carpets is madder red, which is obtained from the so-called dyer madder, which has its area of distribution in the Middle East and the eastern Mediterranean. In addition, blue is extracted from the indigo plant, which is now common as the basic colour of jeans, and yellow is often extracted from chamomile. Almost all nuances of the entire colour spectrum can be produced by mixing colours." + '<p>' + '<br>' +
 
-    "Patterns" + '<br>' + "Persian carpets can show the widest variety of patterns and are often characterized by the place of origin. Often there is a medallion in the middle, which builds up the carpet from the centre. However, a major part is often left to the creativity of the weaver." + '<p>',
+     '<b>' + "Patterns" + '</b>' + '<p>' + "Persian carpets can show the widest variety of patterns and are often characterized by the place of origin. Often there is a medallion in the middle, which builds up the carpet from the centre. However, a major part is often left to the creativity of the weaver." + '<p>',
 ];
 
 document.getElementById("Info").innerHTML = carpets;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
